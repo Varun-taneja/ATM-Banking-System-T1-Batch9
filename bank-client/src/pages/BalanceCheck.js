c5d435fbffcfe07cf6ef55a2bd0741db2915783d
Nallamothu-venugopal <nallamothuvenugopal@gmail.com>
2023-08-08 14:49:00 +0530

    Pages initialization

---
+++ bank-client/src/pages/BalanceCheck.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BalanceCheck(){
+    return (
+        <React.Fragment>
+            <h1>Balance of the customer</h1>
+        </React.Fragment>
+    )
+}
+
+export default BalanceCheck;
---
+++ bank-client/src/pages/CustomerCardDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CustomerCardDetails(){
+    return (
+        <React.Fragment>
+            <h1>Customer card</h1>
+        </React.Fragment>
+    )
+}
+
+export default CustomerCardDetails;
