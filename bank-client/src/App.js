1ea42a49e8b4d27b78a2967e6c17e172a497b9cd
Pranav Somaiah <pranav.somaiah24@gmail.com>
2023-08-08 14:48:35 +0530

    added links to pages

--- bank-client/src/App.js
+++ bank-client/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='gridContainer'>
+      <Link to="create">
+        <button >Create Customer</button>
+      </Link>
+      <Link to="create">
+        <button >View Customer</button>
+      </Link>
+      </div>
     </div>
   );
 }
