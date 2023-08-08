69ee28bce64a3fe16dc01b4af6808a9a14fa698a
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2023-08-08 14:37:59 +0530

    added ViewCustomer.js and AddCustomer.js

---
+++ bank-client/src/pages/AddCustomerDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddCustomerDetails() {
+    return (
+        <React.Fragment>
+            <h1>Add/Edit Customer Details</h1>
+        </React.Fragment>
+    )
+}
+
+export default AddCustomerDetails;
---
+++ bank-client/src/pages/ViewCustomer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ViewCustomer() {
+    return (
+        <React.Fragment>
+            <h1>View Customer details</h1>
+        </React.Fragment>
+    )
+}
+
+export default ViewCustomer;
