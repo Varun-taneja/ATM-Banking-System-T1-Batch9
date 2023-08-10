a8c2c1ccc86fb8862360dd865911368fc4465f9d
Varun-taneja <vtanejayoo@gmail.com>
2023-08-10 10:09:01 +0530

    navbar

--- bank-client/src/App.js
+++ bank-client/src/App.js
@@ -1,27 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Snavbar from './components/Snavbar'
 import { Link } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <div className='gridContainer'>
-      <Link to="create-customer-details">
-        <button >Create Customer</button>
-      </Link>
-      <Link to="edit-customer-details">
-        <button >Edit Customer</button>
-      </Link>
-      <Link to="transaction-details">
-        <button >View Transactions</button>
-      </Link>
-      <Link to="customer-card-details">
-        <button >View Card Details</button>
-      </Link>
-      <Link to="balance">
-        <button >View Balance</button>
-      </Link>
-      </div>
-    </div>
+    <Snavbar/>
+
+    // <div className="App">
+    //   <div className='gridContainer'>
+    //   <Link to="create-customer-details">
+    //     <button >Create Customer</button>
+    //   </Link>
+    //   <Link to="edit-customer-details">
+    //     <button >Edit Customer</button>
+    //   </Link>
+    //   <Link to="transaction-details">
+    //     <button >View Transactions</button>
+    //   </Link>
+    //   <Link to="customer-card-details">
+    //     <button >View Card Details</button>
+    //   </Link>
+    //   <Link to="balance">
+    //     <button >View Balance</button>
+    //   </Link>
+    //   </div>
+    // </div>
   );
 }
 
