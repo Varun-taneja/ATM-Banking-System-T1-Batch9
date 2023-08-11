aa7b6319d5bde2f74288e88140dbe4db0b4d3b50
pranav <pranav.somaiah24@gmail.com>
2023-08-11 12:11:59 +0530

    updated serivces

---
+++ ATM-BS/ATM-BS.API/Data/ATMBSDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ATM_BS.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATM_BS.API.Data
+{
+    public class ATMBSDbContext : DbContext
+    {
+        //public ATMBSDbContex(DbContextOptions<ATMBSDbContex> options) : base(options)
+        //{
+
+        //}
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Balance> Balances { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=WINDOWS-BVQNF6J;Initial Catalog=master;Persist Security Info=True;User ID=sa;Password=12345;TrustServerCertificate=True;");
+        }
+    }
+}
