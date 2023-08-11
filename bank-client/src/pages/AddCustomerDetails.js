01e5382f0107667038fff46da4f45c05aa2537b1
Varun-taneja <69303551+Varun-taneja@users.noreply.github.com>
2023-08-11 13:43:13 +0530

    added customer form links

--- bank-client/src/pages/AddCustomerDetails.js
+++ bank-client/src/pages/AddCustomerDetails.js
@@ -1,16 +1,127 @@
 import React from 'react';
 import Snavbar from '../components/Snavbar'
 import "../Div.css"
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 import Nform from '../components/Nform';
 import Searchbar from '../components/Searchbar';
-
+const axios = require('axios').default;
 function AddCustomerDetails() {
+    // const emailRef = useRef()
+    // const contactRef = useRef()
+    // const addressRef = useRef()
+    // const pincodeRef = useRef()
+    // const NameRef = useRef()
+    // const accountNumRef = useRef()
+    // const custIdRef = useRef()
+    // const accountType = useRef()
+    // const [loading, setLoading] = useState(false)
+    // const navigate = useNavigate()
+
+    
+
+    // const sendRequest = async () => {
+    //     try {
+    //         const resp = await axios({
+    //             method: 'PUT',
+    //             url: '',
+    //             data: {
+    //                 id: 1,
+    //                 userId: 1,
+    //                 title: 'A new title',
+    //                 body: 'Update this post'
+    //             }
+    //         });
+
+    //         console.log(resp.data);
+    //     } catch (err) {
+    //         // Handle Error Here
+    //         console.error(err);
+    //     }
+    // }
+
+
     return (
         <div className='rowC'>
             <Snavbar />
             <div className='colnC'>
                 <div className='searchC'><Searchbar/></div>
-                <div className="formC"><Nform /></div>
+                <div className="formC">
+                    <Form>
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridContact">
+                            <Form.Label>Contact Number</Form.Label>
+                            <Form.Control type="Contact" placeholder="xxxxxxxxxx" />
+                            </Form.Group>
+                        </Row>
+                            
+                        <Form.Group className="mb-3" controlId="formGridAddress1">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control placeholder="Full Address" />
+                        </Form.Group>
+
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control />
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>State</Form.Label>
+                            <Form.Select defaultValue="Choose...">
+                                <option>Choose...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                            </Form.Select>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridPincode">
+                            <Form.Label>Pincode</Form.Label>
+                            <Form.Control />
+                            </Form.Group>
+                        </Row>
+
+                        <Form.Group className="mb-3" id="formGridCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                        </Form>
+                </div>
             </div>
             {/* <h1>Add/Edit Customer Details</h1> */}
         </div>
--- bank-client/src/pages/CreateCustomer.js
+++ bank-client/src/pages/CreateCustomer.js
@@ -1,13 +1,153 @@
-import React from "react";
+import React, { useState } from "react";
 import Snavbar from '../components/Snavbar'
 import "../Div.css"
-import Nform from "../components/Nform";
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+// import Nform from "../components/Nform";
+// import Searchbar from '../components/Searchbar';
+const axios = require('axios').default;
 
 function CreateCustomer(){
+    const [email, setEmail] = useState("");
+    const [contact, setContact] = useState();
+    const [custId, setCustId] = useState();
+    const [accountNumber, setAccountNumber] = useState();
+    const [address, setAddress] = useState("");
+    const [pincode, setPincode] = useState();
+    const [state, setState] = useState("");
+    const [city, setCity] = useState("");
+
+    async function handleSubmit(e) {
+        const data = {
+            email,
+            contact,
+            custId,
+            accountNumber,
+            address,
+            pincode,
+            state,
+            city
+        }
+        e.preventDefault()
+
+        // if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+        //     return alert("Passwords do not match")
+        // }
+
+        try {
+            console.log(data)
+            const res = await axios.post('', data,{ 
+                headers: {'Content-Type': 'application/json'}})
+            console.log(res)
+            
+        } catch (err) {
+            // alert(err.message.substring(
+            //     err.message.indexOf(":") + 1,
+            //     err.message.lastIndexOf("(")))
+        }
+
+        // setLoading(false)
+    }
+
     return (
         <div className='rowC'>
             <Snavbar />
-            <div className="formC"><Nform /></div>
+            <div className='colnC'>
+                <div className="formC">
+                    <Form>
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridContact">
+                            <Form.Label>Contact Number</Form.Label>
+                            <Form.Control type="Contact" placeholder="xxxxxxxxxx"  value={contact} onChange={(e)=>{setContact(e.target.value)}}/>
+                            </Form.Group>
+                        </Row>
+
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridCustomerId">
+                            <Form.Label>Customer ID</Form.Label>
+                            <Form.Control type="" placeholder="xxxxxxxxxx" value={custId} onChange={(e)=>{setCustId(e.target.value)}}/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridAccountNo">
+                            <Form.Label>Account Number</Form.Label>
+                            <Form.Control type="Contact" placeholder="xxxxxxxxxx" value={accountNumber} onChange={(e)=>{setAccountNumber(e.target.value)}}/>
+                            </Form.Group>
+                        </Row>
+                            
+                        <Form.Group className="mb-3" controlId="formGridAddress1">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control placeholder="Full Address" value={address} onChange={(e)=>{setAddress(e.target.value)}}/>
+                        </Form.Group>
+
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control value={city} onChange={(e)=>{setCity(e.target.value)}}/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control value={state} onChange={(e)=>{setState(e.target.value)}}/>
+                            </Form.Group>
+
+                            {/* <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>State</Form.Label>
+                            <Form.Select defaultValue="Choose...">
+                                <option>Choose...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                                <option>...</option>
+                            </Form.Select>
+                            </Form.Group> */}
+
+                            <Form.Group as={Col} controlId="formGridPincode">
+                            <Form.Label>Pincode</Form.Label>
+                            <Form.Control value={pincode} onChange={(e)=>{setPincode(e.target.value)}} />
+                            </Form.Group>
+                        </Row>
+
+                        <Form.Group className="mb-3" id="formGridCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit" onClick={handleSubmit}>
+                            Submit
+                        </Button>
+                        </Form>
+                </div>
+            </div>
+            {/* <h1>Add/Edit Customer Details</h1> */}
         </div>
     )
 }
