0f7a9154e5bf88666dcdace6c48b2ab5dc12411f
pranav <pranav.somaiah24@gmail.com>
2023-08-11 13:27:04 +0530

    add controllers

---
+++ ATM-BS/ATM-BS.API/Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using ATM_BS.API.Entities;
+using ATM_BS.API.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATM_BS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            this.customerService = customerService; 
+        }
+
+        [HttpPost,Route("AddCustomer")]
+        public IActionResult Add(CustomerDTO customerDTO)
+        {
+            try
+            {
+                Customer customer = new Customer()
+                {
+                    CustomerId = customerDTO.CustomerId,
+                    CustomerName = customerDTO.CustomerName,
+                    AccountType = customerDTO.AccountType,
+                    AccountNumber = customerDTO.AccountNumber,
+                    Address = customerDTO.Address,
+                    Pincode = customerDTO.Pincode,
+                    Email = customerDTO.Email,
+                    Contact = customerDTO.Contact,
+                };
+                customerService.AddCustomer(customer);
+                return StatusCode(200, customerDTO);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ATM-BS/ATM-BS.API/DTOS/CustomerDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace ATM_BS.API.DTOS
+{
+    public class CustomerDTO
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+
+        public string AccountType { get; set; }
+        public string Address { get; set; }
+        public int Pincode { get; set; }
+        public string Email { get; set; }
+        public int Contact { get; set; }
+        public int AccountNumber { get; set; }
+    }
+}
