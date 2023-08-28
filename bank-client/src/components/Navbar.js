578844018fd606c541dddd646b6e1db7376efc0a
Varun-taneja <69303551+Varun-taneja@users.noreply.github.com>
2023-08-28 09:32:12 +0530

    navbar added

--- bank-client/src/components/Navbar.js
+++ bank-client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { NavLink, Link } from 'react-router-dom';
-import './Navbar.css';
+import '../Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
