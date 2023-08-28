6b4e8732fbb03b9c8629377afda1230fac81a2d6
Varun-taneja <69303551+Varun-taneja@users.noreply.github.com>
2023-08-28 09:22:22 +0530

    updated landing

--- bank-client/src/pages/LandingPage.js
+++ bank-client/src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import HeroSection from '../components/HeroSection';
 import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 function LandingPage() {
   return (
     <>
+      <Navbar />
       <HeroSection />
       {/* <Cards /> */}
       <Footer />
